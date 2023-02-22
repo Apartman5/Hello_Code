a102e3cf25983c7e54fcf3f24601d49304238b50
Igor Kazakov <kiw@lefterova.ru>
2023-02-22 18:13:47 +0500

    commit commit commit

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша");
+if(username.ToLower() == "МАША")
 {
-    Console.WriteLine("Ура, это же Маша");
+    Console.WriteLine("Ура, это же МАША");
 }
 else
 {
